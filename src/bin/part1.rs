6809fed084a7471ca0a2cfcce17cb601f6af94f8
Antoine Reversat <a.reversat@gmail.com>
2024-12-07 11:58:07 -0500

    Clean up read_input and use results

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,23 +1,20 @@
 use std::env;
 use std::fs;
 use std::process;
+use anyhow::{Result, bail, Context};
 
-fn read_input(filename: &str) -> Vec<String> {
-    let mut ret: Vec<String> = Vec::new();
-    let content = fs::read_to_string(filename).expect("Unable to read from file.");
-    for line in content.lines() {
-        ret.push(line.trim().to_string());
-    }
-
-    ret
+fn read_input(filename: &str) -> Result<Vec<String>> {
+    let content = fs::read_to_string(filename)?;
+    Ok(content.lines().map(|l| l.trim().to_string()).collect())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("Wrong number of args");
-        process::exit(1);
+        bail!("Wrong number of args");
     }
 
-    let contents = read_input(&args[1]);
+    let contents = read_input(&args[1])?;
+
+    Ok(())
 }
